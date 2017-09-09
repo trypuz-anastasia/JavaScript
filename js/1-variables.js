16b4ba589407cb3837fc5cf00d60ce3f97a56845
Anastasiya Trypuz <trypuz.anastasia@gmail.com>
2017-09-09 09:59:46 +0200

    Variables

---
+++ js/1-variables.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
